282da37de18c23f018d82b545cde492eb3ba6866
Alexis Sonolet <alexis.sonolet@gmail.com>
2020-09-02 00:32:56 +0200

    404 and 500 errors handeled

--- functions/index.js
+++ functions/index.js
@@ -16,19 +16,26 @@ app.set('view engine', 'hbs');
 // Chargement de la page index.html
 app.get('/', function(req, res) {
     res.sendFile(__dirname + '/public/index.html');
-    // res.setHeader('Content-Type', 'text/plain');
-    // res.send('Vous êtes à l\'accueil, que puis-je pour vous ?');
 });
 
-// app.get('/timestamp', function(req, res) {
-//     // TODO
-// });
+// Handle Error 404
+app.use(function(req, res) {
+    res.status(400);
+    res.sendFile(__dirname + '/public/404.html');
+});
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+// Handle Error 500
+app.use(function(error, req, res, next) {
+    res.status(500);
+    res.send('500: Internal Server Error', 500);
+    // 500.jade :
+    // extends layout
+    // block content
+    // h1= title
+    // p #{title}
+    // p #{error}
+    //
+    // res.render('500.jade', {title:'500: Internal Server Error', error: error});
+});
 
 exports.app = functions.https.onRequest(app);
