9a4320d800ec0a03ece2feb45daa02db3beb73a3
Alexis Sonolet <alexis.sonolet@gmail.com>
2020-09-15 01:00:06 +0200

    En train de refactorer le code pour inclure handlebars

--- functions/contact-post.js
+++ functions/contact-post.js
@@ -23,3 +23,44 @@ const mailOptions = {
     html: `<h1>Order Confirmation</h1>
      <p> <b>Email: </b>${snap.data().email} </p>`
 };
+
+
+// === Contact form ===
+app.use(bodyParser.urlencoded({ extended: true })); 
+app.post('/contact-post', function(req, res) {
+    console.log('You sent the name "' + req.body.name + '".');
+});
+
+var transporter = nodemailer.createTransport({
+    host: 'smtp.gmail.com',
+    port: 465,
+    secure: true,
+    auth: {
+        user: '********@gmail.com',
+        pass: '************'
+    }
+});
+
+exports.sendEmail = functions.firestore
+    .document('orders/{orderId}')
+    .onCreate((snap, context) => {
+
+        const mailOptions = {
+            from: `***********`,
+            to: snap.data().email,
+            subject: 'contact form message',
+            html: `<h1>Order Confirmation</h1>
+                                <p>
+                                   <b>Email: </b>${snap.data().email}<br>
+                                </p>`
+        };
+
+
+        return transporter.sendMail(mailOptions, (error, data) => {
+            if (error) {
+                console.log(error)
+                return
+            }
+            console.log("Sent!")
+        });
+    });
--- functions/index.js
+++ functions/index.js
@@ -1,45 +1,55 @@
-const functions = require('firebase-functions');
-const firebase = require('firebase-admin');
+// const functions = require('firebase-functions');
+// const firebase = require('firebase-admin');
 const express = require('express');
-const engines = require('consolidate');
+const path = require('path');
 const bodyParser = require('body-parser');
+const nodemailer = require('nodemailer');
+const handlebars = require('express-handlebars');
 
-const firebaseApp = firebase.initializeApp(
-    functions.config().firebase
-);
+// const firebaseApp = firebase.initializeApp(
+//     functions.config().firebase
+// );
 
 const app = express();
-app.engine('hbs', engines.handlebars);
-app.set('views', './views');
-app.set('view engine', 'hbs');
+
+
+app.set('view engine', 'handlebars');
+app.engine('handlebars', handlebars({
+    layoutsDir: __dirname + '/views/layouts',
+}));
 // var server = require('http').createServer(app);
+
+
+app.use(express.static('public'));
 
 
 // === SERVER ===
 // Chargement de la page index.html
 app.get('/', function(req, res) {
-    res.sendFile(__dirname + '/public/index.html');
+    res.render('main', {layout : 'index'});
 });
 
 
 // === Contact form ===
-app.use(bodyParser.urlencoded({ extended: true })); 
-app.post('/contact-post', function(req, res) {
-    console.log('You sent the name "' + req.body.name + '".');
-});
+// app.use(bodyParser.urlencoded({ extended: true })); 
+// app.post('/contact-post', function(req, res) {
+//     console.log('You sent the name "' + req.body.name + '".');
+// });
 
 
 // === ERRORS ===
 // Handle Error 404
-app.use(function(req, res) {
-    res.status(400);
-    res.sendFile(__dirname + '/public/404.html');
-});
+// app.use(function(req, res) {
+//     res.status(400);
+//     res.sendFile(__dirname + '/public/404.html');
+// });
 
 // Handle Error 500
-app.use(function(error, req, res, next) {
-    res.status(500);
-    res.send('500: Internal Server Error', 500);
-});
+// app.use(function(error, req, res, next) {
+//     res.status(500);
+//     res.send('500: Internal Server Error<br />please contact : alexis.sonolet@gmail.com', 500);
+// });
 
-exports.app = functions.https.onRequest(app);
+
+// exports.app = functions.https.onRequest(app);
+app.listen(5000);
